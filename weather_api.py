5967c7b7a8bba459fb797c3b08ef5bad9022b5c0
Ojaswin Khamkar <93571898+PH3ON1X7@users.noreply.github.com>
2022-03-13 12:07:05 +0530

    Add files via upload

---
+++ weather_api.py
@@ -0,0 +1,18 @@
+# Weather Fetcher using API from https://openweathermap.org/
+import requests
+
+API_KEY = "5f29c697290983d39686bb7af757ef11"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data["weather"][0]["description"]
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+    print("\nWeather:     ", weather)
+    print("Temperature: ", temperature, "C")
+else:
+    print("An ERROR has occured")
